49ff9a239d9cb293e408349103d07f6d63296be1
Fraser Curry <fraserjcurry@gmail.com>
2022-05-02 20:20:48 +0100

    Updated images and script name

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// init Isotope
+var $grid = $('#project-list').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+// checks and unchecks buttons
+$('.button-group-wrap').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'div', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
